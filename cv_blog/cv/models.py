37531228815fcbbb8adfb479297c6f47ae1fddd6
Saul Fernandez <sarony11@gmail.com>
2023-06-14 20:34:41 +0200

    fix: add app_label to the models to avoid further errors

--- cv_blog/cv/models.py
+++ cv_blog/cv/models.py
@@ -4,10 +4,14 @@ from django.db import models
 class CV(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    class Meta:
+        app_label = 'cv'
     # Add more fields as needed
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        app_label = 'cv'
     # Add more fields as needed
