94b9596445d317f88cd715e370da44c6b37d1a93
Saul Fernandez <sarony11@gmail.com>
2023-06-14 20:41:38 +0200

    feat: Create URLs for blog post detail view

--- cv_blog/cv/urls.py
+++ cv_blog/cv/urls.py
@@ -6,4 +6,5 @@ app_name = 'cv'
 urlpatterns = [
     path('cv/', views.cv_view, name='cv'),
     path('blog/', views.blog_post_list_view, name='blog_post_list'),
+    path('blog/<int:pk>/', views.blog_post_detail_view, name='blog_post_detail'),
 ]
