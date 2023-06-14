9adc54dc2915d6033e46dee9c551d6cbbbefae29
Saul Fernandez <sarony11@gmail.com>
2023-06-14 20:25:49 +0200

    feat: add urls mapping

---
+++ cv_blog/cv/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'cv'
+
+urlpatterns = [
+    path('cv/', views.cv_view, name='cv'),
+    path('blog/', views.blog_post_list_view, name='blog_post_list'),
+]
